7d43a8c7d1f2d659a223476dfee615eedcb1a79e
Ibtesam Ahmad <zlac459@cim-ts-node-03.cc.rhul.local>
2023-12-29 16:17:22 +0000

    Implemented and tested stackAllocator() and deallocator()

--- step2.c
+++ step2.c
@@ -1,68 +1,4 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#define BUSY '+'
-#define FREE '_'
-#define BUSYSIZE -1
-#define FREESIZE 0
-
-struct space {
-    char *memory;
-    int *sizes;
-    int len;
-};
-
-/* Auxiliary functions */
-
-void printMemory(struct space *mem) {
-    int i = 0;
-    while (i < mem->len) {
-		printf("%c", *(mem->memory + i));
-        i = i + 1;
-    }
-	printf("\n");
-}
-
-void printSizes(struct space *mem) {
-    int i = 0;
-    int c;
-	while (i < mem->len) {
-        int n = *(mem->sizes + i);
-        int t = 10000;
-        while (n > 9) {
-            c = n/t;
-            n = n - c * t;
-            t = t / 10;
-            if (c) {
-                c =  c % 10 + '0';
-				printf("%c", c);
-                i = i + 1;
-            }
-        }   
-		c =  n % 10 + '0';
-		printf("%c", c);
-        i = i + 1;
-    }
-	printf("\n");
-}
-
-void copyString(char *sIn, char *sOut, int len) {
-    int t = 0;
-    while (t < len) {
-        *(sOut + t) = *(sIn + t);
-        t = t + 1;
-    }
-}
-
-int stringLen(char *s) {
-    int t = 0;
-    while (*(s + t) != '\0')
-        t++;
-    return t;
-}
-
-/* Step 1 */
+#include <functions.c>
 
 void initializeMemory(int memSize, struct space *mem) {
     mem->memory = malloc(memSize * sizeof(char));
@@ -90,8 +26,6 @@ void cleanMemory(struct space *mem) {
     free(mem->memory);
     free(mem->sizes);
 }
-
-/* Step 2 */
 
 int stackAllocator(int nbytes, struct space *mem) {
     int t0 = 0;
